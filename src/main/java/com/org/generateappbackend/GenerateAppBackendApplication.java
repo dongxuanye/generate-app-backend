aa85dde1a41377ffa795aec11a0e5463981c4642
dongxuan-night <3066466625@qq.com>
2025-09-21 16:09:22 +0800

    完成后端初始化

---
+++ src/main/java/com/org/generateappbackend/GenerateAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.org.generateappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenerateAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenerateAppBackendApplication.class, args);
+    }
+
+}
