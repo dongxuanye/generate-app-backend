d76d3cf1109c6d08939233ba21c22c41792adff1
dongxuan-night <3066466625@qq.com>
2025-09-24 16:02:08 +0800

    用户模块写完(9.24)

--- src/main/java/com/org/generateappbackend/GenerateAppBackendApplication.java
+++ src/main/java/com/org/generateappbackend/GenerateAppBackendApplication.java
@@ -1,11 +1,13 @@
 package com.org.generateappbackend;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
+@MapperScan("com.org.generateappbackend.mapper")
 public class GenerateAppBackendApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/org/generateappbackend/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.org.generateappbackend.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.org.generateappbackend.model.entity.User;
+
+/**
+ *  映射层。
+ *
+ * @author konna
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
